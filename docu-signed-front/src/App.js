5963eed4d3577daa8648fb477223c76ccf3c7ff1
Irina Poposki <ninapoposki6@gmail.com>
2025-04-10 14:09:57 +0200

    implement login page

--- docu-signed-front/src/App.js
+++ docu-signed-front/src/App.js
@@ -1,23 +1,31 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import axios from "axios";
+import LoginPage from "./pages/auth/LoginPage.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   useEffect(() => {
-    axios.get('http://localhost:5000')
-      .then(response => {
-        console.log('Connection successful:', response.data);
+    axios
+      .get("http://localhost:5000")
+      .then((response) => {
+        console.log("Connection successful:", response.data);
       })
-      .catch(error => {
-        console.error('Error during connection:', error);
+      .catch((error) => {
+        console.error("Error during connection:", error);
       });
   }, []);
 
   return (
-    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
-      <h1>Docku Signed Frontend</h1>
-    </div>
+    <Router>
+      {/* <div style={{ textAlign: "center", paddingTop: "50px" }}>
+        <h1>Docku Signed Frontend</h1> */}
+      <Routes>
+        <Route path="/" element={<h2>Welcome</h2>} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      {/* </div> */}
+    </Router>
   );
 }
 
 export default App;
-
